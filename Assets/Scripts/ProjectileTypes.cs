d341ef5138867961e954a2d0749c663ad46077db
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-03-01 04:47:40 +0000

    Create scriptable object for projectiles and type enum

---
+++ Assets/Scripts/ProjectileTypes.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace RPS_DJDIII.Assets.Scripts
+{
+    [Flags]
+    public enum ProjectileTypes
+    {
+        DEFAULT = 0,
+        ROCK = 1,
+        PAPER = 2,
+        SCISSORS = 4
+
+
+    }
+}
---
+++ Assets/Scripts/scriptables/ProjectileData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using RPS_DJDIII.Assets.Scripts;
+
+[CreateAssetMenu(fileName = "Projectile", 
+    menuName = "Projectile/ProjectileData", order = 0)]
+public class ProjectileData : ScriptableObject
+{
+    [Header("Specs")]
+    [SerializeField] private ProjectileTypes Type;
+    [SerializeField] private ProjectileTypes LosesToType;
+    [SerializeField] private float Velocity; 
+
+
+    [Header("Rendering")]
+    [SerializeField] private Mesh Projectilemesh;
+    [SerializeField] private Material MeshMaterial;
+
+    [Header("VFX")]
+    [SerializeField] private ParticleSystem TrailParticles;
+    [SerializeField] private ParticleSystem BirthPartciles;
+    [SerializeField] private ParticleSystem DeathParticles;
+
+
+
+    
+
+}
