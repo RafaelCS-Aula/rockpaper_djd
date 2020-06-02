ebf161b8ad51dd7efcfc5b4bb6075ad1311fe8fd
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-06-02 05:51:22 +0100

    make SpawnerBehaviour class

---
+++ Assets/Scripts/Behaviours/SpawnerBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: this
+public class SpawnerBehaviour : MonoBehaviour, IDataUser<ManaPickupData>
+{
+    [SerializeField] private ManaPickupData _dataHolder;
+    public ManaPickupData DataHolder
+    {
+        get => _dataHolder;
+        set => value = _dataHolder;
+    }
+
+    
+    public void GetData()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
