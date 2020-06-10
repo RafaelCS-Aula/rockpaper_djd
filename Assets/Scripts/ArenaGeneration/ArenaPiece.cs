2ac9b1e633306bf7f176fb42574830ef5269b6d9
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-06-10 05:18:01 +0100

    Custom sorting to sort by largest connector group

--- Assets/Scripts/ArenaGeneration/ArenaPiece.cs
+++ Assets/Scripts/ArenaGeneration/ArenaPiece.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArenaPiece : MonoBehaviour
+public class ArenaPiece : MonoBehaviour, IComparable<ArenaPiece>
 {
 
     public List<ConnectorGroup> connectorGroups;
-    
+
+
 
     public void Setup()
     {
         connectorGroups.Sort();
 
+    }
+
+    public int CompareTo(ArenaPiece other)
+    {
+        if (this.connectorGroups[0].connectorCount >
+            other.connectorGroups[0].connectorCount)
+            return -1;
+        else if (this.connectorGroups[0].connectorCount <
+            other.connectorGroups[0].connectorCount)
+            return 1;
+        else
+            return 0;
     }
 }
