202da174472db198bbee14fef23eda9abbfa1bc6
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-06-11 06:05:38 +0100

    Comparison against other piece's connectors

--- Assets/Scripts/ArenaGeneration/ArenaPiece.cs
+++ Assets/Scripts/ArenaGeneration/ArenaPiece.cs
@@ -6,26 +6,86 @@ using UnityEngine;
 public class ArenaPiece : MonoBehaviour, IComparable<ArenaPiece>
 {
 
-    private List<ConnectorGroup> connectorGroups;
+    private List<ConnectorGroup> sideConnectorGroups;
     public int largestGroupCount;
     public int smallestGroupCount;
-    public bool isFull = false;
+
 
     public void Setup()
     {
-        connectorGroups.Sort();
-        largestGroupCount = connectorGroups[0].connectorCount;
+        sideConnectorGroups.Sort();
+        largestGroupCount = sideConnectorGroups[0].connectorCount;
         smallestGroupCount = 
-            connectorGroups[connectorGroups.Count - 1].connectorCount;
+            sideConnectorGroups[sideConnectorGroups.Count - 1].connectorCount;
+    }
+
+    public bool isFull()
+    {
+        foreach(ConnectorGroup c in sideConnectorGroups)
+            if(!c.isUsed)
+                return false;
+        return true;
+
+    }
+
+    public (bool valid, Transform positionRot) EvaluatePiece(ArenaPiece other)
+    {
+        //TODO: Check for intersecting geometry
+
+        foreach(ConnectorGroup co in other.sideConnectorGroups)
+        {
+            foreach(ConnectorGroup ct in this.sideConnectorGroups)
+            {
+                if(!co.isUsed && !ct.isUsed && 
+                    co.connectorCount == ct.connectorCount)
+                    {
+                        co.isUsed = true;
+                        return (true, TransformPiece(ct, co, other));
+                    }
+                
+
+                else 
+                    return (false, null);
+            }
+
+        }
+        return (false, null);
+    }
+
+    /// <summary>
+    /// Gets the correct position and rotation of the other piece so its
+    /// connector group matches this piece's.
+    /// </summary>
+    /// <param name="myConnectorGroup"></param>
+    /// <param name="otherConnectorGroup"></param>
+    /// <param name="otherPiece"></param>
+    /// <returns></returns>
+    private Transform TransformPiece(ConnectorGroup myConnectorGroup, ConnectorGroup otherConnectorGroup, ArenaPiece otherPiece)
+    {
+        otherPiece.transform.position = myConnectorGroup.transform.position;
+        otherPiece.transform.position -= (
+            otherConnectorGroup.transform.position - 
+            otherPiece.transform.position);
+
+        
+
+        Vector3 axi = 
+            otherConnectorGroup.orientation == ConnectorGroupTypes.SIDE ?
+                transform.up : transform.forward;
+        
+        //TODO: Get angle between the heading vectors of the connectorgroups
+        //otherPiece.transform.RotateAround(otherConnectorGroup, axi,  )
+
+        
     }
 
     public int CompareTo(ArenaPiece other)
     {
-        if (this.connectorGroups[0].connectorCount >
-            other.connectorGroups[0].connectorCount)
+        if (this.sideConnectorGroups[0].connectorCount >
+            other.sideConnectorGroups[0].connectorCount)
             return -1;
-        else if (this.connectorGroups[0].connectorCount <
-            other.connectorGroups[0].connectorCount)
+        else if (this.sideConnectorGroups[0].connectorCount <
+            other.sideConnectorGroups[0].connectorCount)
             return 1;
         else
             return 0;
