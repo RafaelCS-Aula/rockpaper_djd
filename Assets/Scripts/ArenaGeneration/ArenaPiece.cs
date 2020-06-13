9547cfc941cdf465228fa31912251353b005a13a
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-06-13 04:23:21 +0100

    Union of pieces at connector groups

--- Assets/Scripts/ArenaGeneration/ArenaPiece.cs
+++ Assets/Scripts/ArenaGeneration/ArenaPiece.cs
@@ -28,7 +28,8 @@ public class ArenaPiece : MonoBehaviour, IComparable<ArenaPiece>
 
     }
 
-    public (bool valid, Transform positionRot) EvaluatePiece(ArenaPiece other)
+    public (bool valid, Transform positionRot) EvaluatePiece(
+        ArenaPiece other, float pieceDistance = 0.0f)
     {
         //TODO: Check for intersecting geometry
 
@@ -40,7 +41,12 @@ public class ArenaPiece : MonoBehaviour, IComparable<ArenaPiece>
                     co.connectorCount == ct.connectorCount)
                     {
                         co.isUsed = true;
-                        return (true, TransformPiece(ct, co, other));
+                        return (true, TransformPiece(
+                            ct,
+                            co, 
+                            other, 
+                            pieceDistance)
+                        );
                     }
                 
 
@@ -60,22 +66,36 @@ public class ArenaPiece : MonoBehaviour, IComparable<ArenaPiece>
     /// <param name="otherConnectorGroup"></param>
     /// <param name="otherPiece"></param>
     /// <returns></returns>
-    private Transform TransformPiece(ConnectorGroup myConnectorGroup, ConnectorGroup otherConnectorGroup, ArenaPiece otherPiece)
+    private Transform TransformPiece(ConnectorGroup myConnectorGroup, ConnectorGroup otherConnectorGroup, ArenaPiece otherPiece, 
+    float offset)
     {
-        otherPiece.transform.position = myConnectorGroup.transform.position;
-        otherPiece.transform.position -= (
-            otherConnectorGroup.transform.position - 
-            otherPiece.transform.position);
+        Transform newPieceTrn = otherConnectorGroup.transform;
+        Quaternion connectorPointRotation = new Quaternion();
 
-        
+        // Put the other piece on my connector
+        newPieceTrn.position = myConnectorGroup.transform.position;
 
-        Vector3 axi = 
-            otherConnectorGroup.orientation == ConnectorGroupTypes.SIDE ?
-                transform.up : transform.forward;
-        
-        //TODO: Get angle between the heading vectors of the connectorgroups
-        //otherPiece.transform.RotateAround(otherConnectorGroup, axi,  )
+        // temprarily revert parenting so we can move the connector
+        // group and have the geometry follow.
+        otherConnectorGroup.transform.SetParent(null, true);
+        otherPiece.transform.SetParent(otherConnectorGroup.transform, true);
 
+        // Have the other connector group look towards my connector group
+        connectorPointRotation.SetLookRotation(
+            -myConnectorGroup.transform.forward,
+             otherConnectorGroup.transform.up);
+
+        // Apply the rotation acquired above
+        newPieceTrn.rotation = connectorPointRotation;
+
+        // move the connectors away from each other based on an offset
+        newPieceTrn.position -= newPieceTrn.forward * offset;
+
+        // get the parenting back to normal to safeguard future transformations.
+        otherConnectorGroup.transform.SetParent(null, true);
+        otherPiece.transform.SetParent(otherConnectorGroup.transform, true);   	
+
+        return newPieceTrn;
         
     }
 
