c7410cfbc9af4f67fc68b8d3dcb6cf48ff0d9985
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-06-15 06:11:07 +0100

    Add Gizmo for easy editing of connector group

--- Assets/Scripts/ArenaGeneration/ConnectorGroup.cs
+++ Assets/Scripts/ArenaGeneration/ConnectorGroup.cs
@@ -6,9 +6,7 @@ using UnityEngine;
 
 public class ConnectorGroup : MonoBehaviour, IComparable<ConnectorGroup>
 {
-
-    //[SerializeField] private List<Connector> _connectors;
-    public ConnectorGroupTypes orientation;
+    private ConnectorGroupTypes orientation;
     public Vector3 heading => orientation == ConnectorGroupTypes.SIDE ?  transform.forward : orientation == ConnectorGroupTypes.TOP ? transform.up : -transform.up;
 
 
@@ -16,35 +14,20 @@ public class ConnectorGroup : MonoBehaviour, IComparable<ConnectorGroup>
 
     public int connectorCount = 0;
 
+    [SerializeField]private  float connectorSpacing = 0.5f;
+
+
+    [SerializeField] private Color _gizmoColor;
+
     private void Awake()
     {
         //GetUnionPoint();
         /*foreach (Connector c in _connectors)
             c.groupColor = gizmoColor;
           */  
+        
     }
 
-    /*public Vector3 GetUnionPoint()
-    {
-        // Formula for Weighted Average:
-        // (Sum(Value * weight)) / Sum(Weights) 
-
-        Vector3 sum = Vector3.zero;
-        float sumOfWeights = 0;
-
-        foreach (Connector c in _connectors)
-        {
-            sum += c.transform.position * c.weight;
-            sumOfWeights += c.weight;
-            
-        }
-        centreOfMass = sum / sumOfWeights;
-
-        print(centreOfMass);
-        return centreOfMass;
-    }*/
-
-    //public int GetConnectorCount() => _connectors.Count;
 
     public int CompareTo(ConnectorGroup other)
     {
@@ -57,17 +40,34 @@ public class ConnectorGroup : MonoBehaviour, IComparable<ConnectorGroup>
             return 0;
     }
 
-    /* private void OnDrawGizmos()
+     private void OnDrawGizmos()
      {
-         if (Application.isPlaying)
+         //TODO: make gizmo have random color
+
+
+ 
+        Gizmos.DrawLine(transform.position, transform.position + heading);
+
+        Vector3 pos;
+
+         for(float i = 0 - connectorCount / 2; i <=  connectorCount / 2; i++)
          {
-             Gizmos.color = gizmoColor;
-             Vector3 sum = Vector3.zero;
+             if(connectorCount % 2 == 0 && i == 0)
+             {
 
+                continue;
 
-             Gizmos.DrawCube(centreOfMass, new Vector3(10,10,10));
+             }
+             //pos.x = transform.position.x + (i * connectorSpacing);
+             pos = transform.position + transform.right * i * connectorSpacing;
+             //pos.z = transform.position.z * transform.right.z  + (i * connectorSpacing);
+             Gizmos.DrawWireCube(pos , new Vector3(0.5f,
+              0.5f,
+               connectorSpacing) );
 
          }
 
-     }*/
+
+        
+     }
 }
